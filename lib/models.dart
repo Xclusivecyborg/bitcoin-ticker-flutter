0ef5848f36e9a0f6ff59a30e4719efd2ba5b13b2
Xclusivecyborg <Xclusivecyborg@gmail.com>
2021-09-08 10:48:47 +0100

    Updated the UI with more currencies

---
+++ lib/models.dart
@@ -0,0 +1,43 @@
+// To parse this JSON data, do
+//
+//     final myCoinData = myCoinDataFromJson(jsonString);
+
+import 'dart:convert';
+
+MyCoinData myCoinDataFromJson(String str) =>
+    MyCoinData.fromJson(json.decode(str));
+
+String myCoinDataToJson(MyCoinData data) => json.encode(data.toJson());
+
+class MyCoinData {
+  MyCoinData({
+    this.time,
+    this.assetIdBase,
+    this.assetIdQuote,
+    this.rate,
+  });
+
+  DateTime time;
+  String assetIdBase;
+  String assetIdQuote;
+  double rate;
+
+  factory MyCoinData.fromJson(Map<String, dynamic> json) => MyCoinData(
+        time: DateTime.parse(json["time"]),
+        assetIdBase: json["asset_id_base"],
+        assetIdQuote: json["asset_id_quote"],
+        rate: json["rate"].toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "time": time.toIso8601String(),
+        "asset_id_base": assetIdBase,
+        "asset_id_quote": assetIdQuote,
+        "rate": rate,
+      };
+
+  @override
+  String toString() {
+    return 'Crypto time: $time, assetIdBase: $assetIdBase, assetIdQuote: $assetIdQuote, rate: $rate';
+  }
+}
